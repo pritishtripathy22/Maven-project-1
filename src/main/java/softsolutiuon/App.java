cfac8c0ec68ed1cfe6e6041d0336ac1beb195864
pritishtripathy22 <pritishofficer22@gmail.com>
2022-02-04 16:15:43 +0530

    the commit for maven is done

---
+++ src/main/java/softsolutiuon/App.java
@@ -0,0 +1,13 @@
+package softsolutiuon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( maven for devops!" );
+    }
+}
